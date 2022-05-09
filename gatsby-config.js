19b4b2bccee349ada66e6e784804cf16b38ff33f
Wullems Bart <wullemsb@gmail.com>
2022-05-09 18:33:19 +0200

    Customized info

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Wolf's Wishlist`,
+    siteTitleAlt: `Wolf's Wishlist`,
+    siteHeadline: `Wolf's Wishlist`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-    siteLanguage: `en`,
+    siteDescription: `Wolf's Wishlist`,
+    siteLanguage: `nl`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
   },
